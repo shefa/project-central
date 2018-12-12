67830322ed1addd617a6f30b7ba32253aa4f76ce
Georgi Marinov <venom1724@gmail.com>
2018-12-12 23:49:36 +0000

    default accounts

--- app/client/controllers/main.js
+++ app/client/controllers/main.js
@@ -3,9 +3,11 @@ import '../views/main.html';
 
 Template.menu.helpers({
   availableTabs() {
-    if(Meteor.user().profile['client']) return ['Project', 'Progress', 'Meetings'];
-    if(Meteor.user().profile['leader']) return ['Assessment', 'Groups', 'Projects'];
-    if(Meteor.user().profile['supervisor']) return ['Groups', 'Contribution'];
+  	if(Meteor.user().profile){
+	    if(Meteor.user().profile['leader']) return ['Assessment', 'Groups', 'Projects','Students','Clients'];
+	    if(Meteor.user().profile['client']) return ['Project', 'Progress', 'Meetings'];
+	    if(Meteor.user().profile['supervisor']) return ['Groups', 'Contribution'];
+	}
     return ['Brief','Coursework','Files','Schedule','Log','Tools'];
   },
 });
--- app/server/main.js
+++ app/server/main.js
@@ -2,4 +2,38 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  if(Meteor.users.find().count()<4)
+  {
+  	console.log("Creating testing accounts of each kind");
+  	
+	Accounts.createUser( {
+	    email:  'leader@test.com',
+	    password: '123leader',
+	    profile: {
+	        leader: true
+	    }
+	});
+
+	Accounts.createUser( {
+	    email:  'client@test.com',
+	    password: '123client',
+	    profile: {
+	        client: true
+	    }
+	});
+
+	Accounts.createUser( {
+	    email:  'supervisor@test.com',
+	    password: '123supervisor',
+	    profile: {
+	        supervisor: true
+	    }
+	});
+
+	Accounts.createUser( {
+	    email:  'student@test.com',
+	    password: '123student',
+	    profile: {}
+	});
+  }
 });
